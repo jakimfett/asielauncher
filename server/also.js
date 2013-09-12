dac8c84bf3e7789665a635a740686d2967372c9f
asiekierka <asiekierka@gmail.com>
2013-09-12 17:35:49 +0200

    Refactor server

--- server/also.js
+++ server/also.js
@@ -3,12 +3,9 @@ var express = require('express')
   , _ = require('underscore')
   , fs = require('fs')
   , wrench = require('wrench')
-  , util = require('util')
+  , util = require('./util.js')
   , Zip = require('adm-zip')
-  , crypto = require('crypto')
-  , argv = require('optimist').argv
-  , ansi = require('ansi')
-  , cursor = ansi(process.stdout);
+  , argv = require('optimist').argv;
 
 var app = express()
   , config = require("./AsieLauncher/config.json")
@@ -16,16 +13,13 @@ var app = express()
   , DEBUG = false
   , DO_LOCAL = false;
 
-var textColors = {
-  "debug": "grey",
-  "info": "brightWhite",
-  "warning": "brightYellow",
-  "error": "brightRed"
-};
+config.client_revision = 5;
 
 // Handle parameters
 DEBUG = argv.d || argv.debug || false;
 DO_LOCAL = argv.l || argv.local || false;
+
+util.setDebug(DEBUG);
 
 if(DO_LOCAL) {
   if(fs.existsSync("./ALWebFiles")) {
@@ -34,7 +28,7 @@ if(DO_LOCAL) {
 }
 
 function addDirectory(dirName, realDir) {
-  say("debug", "addDirectory: " + dirName + " <- " + realDir);
+  util.say("debug", "addDirectory: " + dirName + " <- " + realDir);
   if(DO_LOCAL) {
     wrench.copyDirSyncRecursive(realDir+"/", './ALWebFiles/'+dirName+"/", {
       excludeHiddenUnix: false
@@ -44,15 +38,6 @@ function addDirectory(dirName, realDir) {
   }
 }
 addDirectory("", "./AsieLauncher/htdocs");
-
-config.client_revision = 5;
-
-function say(type, message) {
-  if(type == "debug" && !DEBUG) return;
-  cursor[textColors[type]]() // Set color
-        .bold().write("["+type.toUpperCase()+"]")
-        .reset().write(" "+message+'\n');
-}
 
 function getDirectoryList(name, destName, addLocation, prefix) {
   if(!_.isString(prefix)) prefix = "";
@@ -64,14 +49,14 @@ function getDirectoryList(name, destName, addLocation, prefix) {
             _.each(config.blacklist, function(test) {
               if(location.indexOf(test) >= 0) {
                 blacklisted = true;
-                say("info", "Blacklisted "+file);
+                util.say("info", "Blacklisted "+file);
               }
             });
             if(blacklisted) return false;
             else return !(fs.lstatSync(name+"/"+file).isDirectory());
           })
           .map(function(file) {
-            var fileNew = {"filename": prefix+file, "size": getSize(name+"/"+file), "md5": md5(name+"/"+file)};
+            var fileNew = {"filename": prefix+file, "size": util.getSize(name+"/"+file), "md5": util.md5(name+"/"+file)};
 						var destination = destName+file;
             if(addLocation) fileNew.location = name+"/"+file;
 				    fileNew.overwrite = !(_.contains(config.noOverwrite, file));
@@ -84,44 +69,18 @@ function getDirectoryList(name, destName, addLocation, prefix) {
           .value();          
 }
 
-function md5(file) {
-  return crypto.createHash('md5').update(fs.readFileSync(file)).digest('hex');
-}
-
-function getSize(file) {
-  return fs.statSync(file).size;
+function getPossibleDirectories(name) {
+  return [name, name+"-client", "./AsieLauncher/"+name, "./AsieLauncher/"+name+"-client"];
 }
 
 function getDirectoriesList(name, addLocation) {
   var destName = name+"/";
-  if(name == "root") destName = "";
-  return _.union(getDirectoryList(name, destName, addLocation),
-                 getDirectoryList(name+"-client", destName, addLocation),
-                 getDirectoryList("AsieLauncher/"+name, destName, addLocation)
-         );
-}
-
-function getDirectoriesSize(name) {
-  var s = 0;
-  _.each(name, function(file) { s += file.size; });
-  return s;
-}
-
-function toUnix(date) {
-  return Math.floor(new Date(date).getTime()/1000);
-}
-
-function sortFilesBySubstring(order, fileList) {
-  return _.sortBy(fileList, function(file) {
-    var i = -1;
-    _.each(order, function(e, index) {
-      if(i>=0) return;
-      if(file.filename.toLowerCase().indexOf(e.toLowerCase()) >= 0) {
-        i = index;
-      }
-    });
-    return i >= 0 ? i : order.length;
+  if(name == "root") destName = ""; // Special case
+  var dirs = [];
+  _.each(getPossibleDirectories(name), function(dir) {
+    dirs = _.union(dirs, getDirectoryList(dir, destName, addLocation));
   });
+  return dirs;
 }
 
 console.log("Welcome to ALSO");
@@ -132,7 +91,7 @@ addDirectory("platform", "./AsieLauncher/platform");
 addDirectory("options", "./AsieLauncher/options");
 addDirectory("jarPatches", "./AsieLauncher/jarPatches");
 
-infoData.jarPatches = sortFilesBySubstring(config.jarPatchOrder,
+infoData.jarPatches = util.sortFilesBySubstrings(config.jarPatchOrder,
                         getDirectoryList("./AsieLauncher/jarPatches", "jarPatches/", false, "jarPatches/")
                       );
 
@@ -149,11 +108,7 @@ infoData.onlineMode = config.onlineMode || false;
 infoData.mcVersion = config.mcVersion || "1.5.2"; // default is 1.5.2
 
 _.each(config.loggedDirs, function(dir) {
-	if(!fs.existsSync(dir) && !fs.existsSync(dir+"-client") && !fs.existsSync("./AsieLauncher/"+dir)) {
-		say("warning", "Directory "+dir+" not found!");
-		return;
-	}
-  say("info", "Adding logged directory "+dir);
+  var found = false;
   var list = getDirectoriesList(dir, false);
   infoData.files = _.union(infoData.files, _.map(list, function(file) {
     file.filename = dir+"/"+file.filename;
@@ -161,50 +116,50 @@ _.each(config.loggedDirs, function(dir) {
     return file;
   }));
   // Express
-  if(fs.existsSync(dir)) addDirectory(dir, "./"+dir);
-  if(fs.existsSync(dir+"-client")) addDirectory(dir, "./"+dir+"-client");
-  if(fs.existsSync("./AsieLauncher/"+dir)) addDirectory(dir, "./AsieLauncher/"+dir);
+  _.each(getPossibleDirectories(dir), function(target) {
+    if(fs.existsSync(dir)) { addDirectory(dir, target); found = true; }
+  });
+  if(found) util.say("info", "Added directory "+dir);
 });
 
 _.each(config.zippedDirs, function(dir) {
-	if(!fs.existsSync(dir) && !fs.existsSync(dir+"-client") && !fs.existsSync("./AsieLauncher/"+dir)) {
-		say("warning", "Directory "+dir+" not found!");
-		return;
-	}
-  say("info", "Adding zipped directory "+dir);
-  var list = getDirectoriesList(dir, true)
+  var found = false
     , zip = new Zip();
-  if(fs.existsSync(dir)) zip.addLocalFolder(dir);
-  if(fs.existsSync(dir+"-client")) zip.addLocalFolder(dir+"-client");
-  if(fs.existsSync("AsieLauncher/"+dir)) zip.addLocalFolder("AsieLauncher/"+dir);
+  _.each(getPossibleDirectories(dir), function(dir){
+    if(!fs.existsSync(dir)) return;
+    found = true;
+    zip.addLocalFolder(dir);
+  });
+  if(!found) return;
+  util.say("info", "Added ZIP directory "+dir);
   zip.writeZip("./AsieLauncher/zips/"+dir+".zip");
-  var zipData = {"filename": dir+".zip", "directory": dir == "root" ? "" : dir, "size": getSize("./AsieLauncher/zips/"+dir+".zip"), "md5": md5("./AsieLauncher/zips/"+dir+".zip"),
-                 "overwrite": !(_.contains(config.noOverwrite, dir)) };
+  var zipData = {"filename": dir+".zip", "directory": dir == "root" ? "" : dir, "size": util.getSize("./AsieLauncher/zips/"+dir+".zip"),
+		 "md5": util.md5("./AsieLauncher/zips/"+dir+".zip"), "overwrite": !(_.contains(config.noOverwrite, dir)) };
   infoData.zips.push(zipData);
   infoData.size += zipData.size;
 });
 
 _.each(fs.readdirSync("./AsieLauncher/platform"), function(platform) {
-  say("debug", "Adding platform "+platform);
+  util.say("debug", "Adding platform "+platform);
   var list = getDirectoryList("./AsieLauncher/platform/"+platform, "", false);
-  var size = getDirectoriesSize(list);
+  var size = util.getFilesSize(list);
   infoData.platforms[platform] = {"files": list, "size": size};
 });
 
 _.each(config.options, function(option) {
   var dir = "./AsieLauncher/options/"+option.id;
   if(!fs.existsSync(dir)) return;
-  say("info", "Adding option "+option.id+" ["+option.name+"]");
+  util.say("info", "Adding option "+option.id+" ["+option.name+"]");
   if(!option.zip) { // Directory
     var list = getDirectoryList(dir, "", false);
-    var size = getDirectoriesSize(list);
+    var size = util.getFilesSize(list);
     infoData.options[option.id]=_.extend(option, {"files": list, "size": size});
   } else { // ZIP
     var zip = new Zip()
       , zipFile = "./AsieLauncher/zips/option-"+option.id+".zip";
     zip.addLocalFolder(dir);
     zip.writeZip(zipFile);
-   infoData.options[option.id]=_.extend(option, {"filename": "option-"+option.id+".zip", "directory": "", "size": getSize(zipFile), "md5": md5(zipFile)});
+   infoData.options[option.id]=_.extend(option, {"filename": "option-"+option.id+".zip", "directory": "", "size": util.getSize(zipFile), "md5": util.md5(zipFile)});
   }
 });
 
@@ -220,5 +175,5 @@ addDirectory("zips", "./AsieLauncher/zips");
 
 if(!DO_LOCAL) {
   app.listen(config.port);
-  say("info", "Ready - listening on port "+config.port+"!");
-} else say("info", "Files exported to ./ALWebFiles/");
+  util.say("info", "Ready - listening on port "+config.port+"!");
+} else util.say("info", "Files exported to ./ALWebFiles/");
---
+++ server/util.js
@@ -0,0 +1,55 @@
+var ansi = require('ansi')
+  , cursor = ansi(process.stdout)
+  , _ = require('underscore')
+  , fs = require('fs')
+  , crypto = require('crypto');
+
+var DEBUG = false;
+
+exports.setDebug = function(d) { DEBUG = d; }
+
+var textColors = {
+  "debug": "grey",
+  "info": "brightWhite",
+  "warning": "brightYellow",
+  "error": "brightRed"
+};
+
+exports.say = function(type, message) {
+  if(type == "debug" && !DEBUG) return;
+  cursor[textColors[type]]() // Set color
+        .bold().write("["+type.toUpperCase()+"]")
+        .reset().write(" "+message+'\n');
+}
+
+exports.getFilesSize = function(files) {
+  var s = 0;
+  _.each(files, function(file) { s += file.size; });
+  return s;
+}
+
+exports.md5 = function(file) {
+  return crypto.createHash('md5').update(fs.readFileSync(file)).digest('hex');
+}
+
+exports.getSize = function(file) {
+  return fs.statSync(file).size;
+}
+
+exports.unix = function(date) {
+  return Math.floor(new Date(date).getTime()/1000);
+}
+
+exports.sortFilesBySubstrings = function(order, fileList) {
+  return _.sortBy(fileList, function(file) {
+    var i = -1;
+    _.each(order, function(e, index) {
+      if(i>=0) return;
+      if(file.filename.toLowerCase().indexOf(e.toLowerCase()) >= 0) {
+        i = index;
+      }
+    });
+    return i >= 0 ? i : order.length;
+  });
+}
+
