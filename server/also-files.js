cdffe104072477f66e7c797190c3bd0336549431
asiekierka <asiekierka@gmail.com>
2013-09-13 12:58:29 +0200

    Major files refactor; may contain bugs!

---
+++ server/also-files.js
@@ -0,0 +1,117 @@
+var _ = require('underscore')
+  , fs = require('fs')
+  , wrench = require('wrench')
+  , util = require('./util.js')
+  , Zip = require('adm-zip');
+	
+var config = {
+	"blacklist": [],
+	"noOverwrite": []
+}
+	
+function getDirectoryList(name, destName, addLocation, prefix) {
+  if(!_.isString(prefix)) prefix = "";
+  if(!fs.existsSync(name)) return [];
+  return _.chain(wrench.readdirSyncRecursive(name))
+          .filter(function(file) {
+            var location = name+"/"+file;
+            var blacklisted = false;
+            _.each(config.blacklist, function(test) {
+              if(location.indexOf(test) >= 0) {
+                blacklisted = true;
+                util.say("info", "Blacklisted "+file);
+              }
+            });
+            if(blacklisted) return false;
+            else return !(fs.lstatSync(name+"/"+file).isDirectory());
+          })
+          .map(function(file) {
+            var fileNew = {"filename": prefix+file, "size": util.getSize(name+"/"+file), "md5": util.md5(name+"/"+file)};
+						var destination = destName+file;
+            if(addLocation) fileNew.location = name+"/"+file;
+				    fileNew.overwrite = !(_.contains(config.noOverwrite, file));
+						if(fileNew.overwrite)
+				      fileNew.overwrite = !(_.some(config.noOverwrite, function(nameNO) {
+				        return (destination.indexOf(nameNO) == 0);
+				      }));
+            return fileNew;
+          })
+          .value();          
+}
+exports.getDirectoryList = getDirectoryList;
+
+function getPossibleDirectories(name) {
+	var dirs = [name, name+"-client", "./AsieLauncher/"+name, "./AsieLauncher/"+name+"-client"];
+	if(name.indexOf(".") === 0) dirs = [name];
+	return _.filter(dirs, fs.existsSync);
+}
+exports.getPossibleDirectories = getPossibleDirectories;
+
+function getDirectoriesList(name, addLocation) {
+  var destName = name+"/";
+  if(name == "root") destName = ""; // Special case
+  var dirs = [];
+  _.each(getPossibleDirectories(name), function(dir) {
+    dirs = _.union(dirs, getDirectoryList(dir, destName, addLocation));
+  });
+  return dirs;
+}
+
+var totalSize = 0;
+
+exports.initialize = function(_config) {
+	totalSize = 0;
+	config = _config;
+	fs.mkdir("AsieLauncher/zips", function(){ });
+}
+
+exports.file = function(dir) {
+  var list = getDirectoriesList(dir, false);
+  return _.map(list, function(file) {
+    file.filename = dir+"/"+file.filename;
+    totalSize += file.size;
+    return file;
+  });
+}
+
+
+function createZip(name, dirs) {
+	var zip = new Zip();
+	var found = false;
+	_.each(dirs, function(dir) {
+		if(!fs.existsSync(dir)) return;
+		found = true;
+		zip.addLocalFolder(dir);
+	});
+	if(found) zip.writeZip(name); // No empty zips!
+	return found;
+}
+exports.createZip = createZip;
+
+exports.zip = function(dir, overwrite) {
+	var name = "./AsieLauncher/zips/"+dir+".zip";
+	if(createZip(name, [dir])) {
+		var zipData = {"filename": dir+".zip", "directory": dir == "root" ? "" : dir,
+			"size": util.getSize(name),
+			"md5": util.md5(name), "overwrite": overwrite || true};
+	  totalSize += zipData.size;
+		return zipData;
+	} else return {};
+}
+
+function getFiles(dir) {
+  var list = getDirectoryList(dir, "", false);
+  var size = util.getFilesSize(list);
+  return {"files": list, "size": size};
+}
+exports.getFiles = getFiles;
+
+exports.platforms = function() {
+	var p = {};
+	_.each(fs.readdirSync("./AsieLauncher/platform"), function(platform) {
+	  p[platform] = getFiles("./AsieLauncher/platform/"+platform);
+	});
+	return p;
+}
+
+exports.getTotalSize = function() { return totalSize; }
--- server/also.js
+++ server/also.js
@@ -4,8 +4,8 @@ var express = require('express')
   , fs = require('fs')
   , wrench = require('wrench')
   , util = require('./util.js')
-  , Zip = require('adm-zip')
-  , argv = require('optimist').argv;
+  , argv = require('optimist').argv
+	, files = require('./also-files.js');
 
 var app = express()
   , config = require("./AsieLauncher/config.json")
@@ -37,68 +37,21 @@ function addDirectory(dirName, realDir) {
     app.use("/" + dirName, express.static(realDir));
   }
 }
+
+// INITIALIZE ROOT DIRECTORY
 addDirectory("", "./AsieLauncher/htdocs");
-
-function getDirectoryList(name, destName, addLocation, prefix) {
-  if(!_.isString(prefix)) prefix = "";
-  if(!fs.existsSync(name)) return [];
-  return _.chain(wrench.readdirSyncRecursive(name))
-          .filter(function(file) {
-            var location = name+"/"+file;
-            var blacklisted = false;
-            _.each(config.blacklist, function(test) {
-              if(location.indexOf(test) >= 0) {
-                blacklisted = true;
-                util.say("info", "Blacklisted "+file);
-              }
-            });
-            if(blacklisted) return false;
-            else return !(fs.lstatSync(name+"/"+file).isDirectory());
-          })
-          .map(function(file) {
-            var fileNew = {"filename": prefix+file, "size": util.getSize(name+"/"+file), "md5": util.md5(name+"/"+file)};
-						var destination = destName+file;
-            if(addLocation) fileNew.location = name+"/"+file;
-				    fileNew.overwrite = !(_.contains(config.noOverwrite, file));
-						if(fileNew.overwrite)
-				      fileNew.overwrite = !(_.some(config.noOverwrite, function(nameNO) {
-				        return (destination.indexOf(nameNO) == 0);
-				      }));
-            return fileNew;
-          })
-          .value();          
-}
-
-function getPossibleDirectories(name) {
-  return [name, name+"-client", "./AsieLauncher/"+name, "./AsieLauncher/"+name+"-client"];
-}
-
-function getDirectoriesList(name, addLocation) {
-  var destName = name+"/";
-  if(name == "root") destName = ""; // Special case
-  var dirs = [];
-  _.each(getPossibleDirectories(name), function(dir) {
-    dirs = _.union(dirs, getDirectoryList(dir, destName, addLocation));
-  });
-  return dirs;
-}
 
 console.log("Welcome to ALSO");
 
-// init folders
-fs.mkdir("AsieLauncher/zips", function(){ });
-addDirectory("platform", "./AsieLauncher/platform");
-addDirectory("options", "./AsieLauncher/options");
-addDirectory("jarPatches", "./AsieLauncher/jarPatches");
+files.initialize(config);
 
 infoData.jarPatches = util.sortFilesBySubstrings(config.jarPatchOrder,
-                        getDirectoryList("./AsieLauncher/jarPatches", "jarPatches/", false, "jarPatches/")
+                        files.getDirectoryList("./AsieLauncher/jarPatches", "jarPatches/", false, "jarPatches/")
                       );
 
 infoData.files = [];
 infoData.zips = [];
 infoData.options = {};
-infoData.platforms = {};
 
 infoData.size = 0;
 infoData.jvmArguments = config.jvmArguments || "";
@@ -108,60 +61,42 @@ infoData.onlineMode = config.onlineMode || false;
 infoData.mcVersion = config.mcVersion || "1.5.2"; // default is 1.5.2
 
 _.each(config.loggedDirs, function(dir) {
-  var found = false;
-  var list = getDirectoriesList(dir, false);
-  infoData.files = _.union(infoData.files, _.map(list, function(file) {
-    file.filename = dir+"/"+file.filename;
-    infoData.size += file.size;
-    return file;
-  }));
-  // Express
-  _.each(getPossibleDirectories(dir), function(target) {
-    if(fs.existsSync(dir)) { addDirectory(dir, target); found = true; }
+	infoData.files = _.union(infoData.files, files.file(dir));
+  var dirs = files.getPossibleDirectories(dir);
+  _.each(dirs, function(target) {
+    addDirectory(dir, target);
+		found = true;
   });
-  if(found) util.say("info", "Added directory "+dir);
+  if(dirs.length > 0) util.say("info", "Added directory "+dir);
 });
 
 _.each(config.zippedDirs, function(dir) {
-  var found = false
-    , zip = new Zip();
-  _.each(getPossibleDirectories(dir), function(dir){
-    if(!fs.existsSync(dir)) return;
-    found = true;
-    zip.addLocalFolder(dir);
-  });
-  if(!found) return;
-  util.say("info", "Added ZIP directory "+dir);
-  zip.writeZip("./AsieLauncher/zips/"+dir+".zip");
-  var zipData = {"filename": dir+".zip", "directory": dir == "root" ? "" : dir, "size": util.getSize("./AsieLauncher/zips/"+dir+".zip"),
-		 "md5": util.md5("./AsieLauncher/zips/"+dir+".zip"), "overwrite": !(_.contains(config.noOverwrite, dir)) };
-  infoData.zips.push(zipData);
-  infoData.size += zipData.size;
+  var zip = files.zip(dir, !_.contains(config.noOverwrite, dir));
+	if(zip === null) return;
+	infoData.zips.push(zip);
+	util.say("info", "Added ZIP directory "+dir);
 });
 
-_.each(fs.readdirSync("./AsieLauncher/platform"), function(platform) {
-  util.say("debug", "Adding platform "+platform);
-  var list = getDirectoryList("./AsieLauncher/platform/"+platform, "", false);
-  var size = util.getFilesSize(list);
-  infoData.platforms[platform] = {"files": list, "size": size};
-});
+infoData.platforms = files.platforms();
 
 _.each(config.options, function(option) {
   var dir = "./AsieLauncher/options/"+option.id;
   if(!fs.existsSync(dir)) return;
   util.say("info", "Adding option "+option.id+" ["+option.name+"]");
   if(!option.zip) { // Directory
-    var list = getDirectoryList(dir, "", false);
-    var size = util.getFilesSize(list);
-    infoData.options[option.id]=_.extend(option, {"files": list, "size": size});
+    infoData.options[option.id] = _.extend(option, files.getFiles(dir));
   } else { // ZIP
-    var zip = new Zip()
-      , zipFile = "./AsieLauncher/zips/option-"+option.id+".zip";
-    zip.addLocalFolder(dir);
-    zip.writeZip(zipFile);
-   infoData.options[option.id]=_.extend(option, {"filename": "option-"+option.id+".zip", "directory": "", "size": util.getSize(zipFile), "md5": util.md5(zipFile)});
+		var name = "./AsieLauncher/zips/option-"+option.id+".zip";
+		if(createZip(name, [dir])) {
+      infoData.options[option.id]=_.extend(option, {"filename": "option-"+option.id+".zip", "directory": "", "size": util.getSize(zipFile), "md5": util.md5(zipFile)});
+		}
   }
 });
+
+addDirectory("zips", "./AsieLauncher/zips");
+addDirectory("platform", "./AsieLauncher/platform");
+addDirectory("options", "./AsieLauncher/options");
+addDirectory("jarPatches", "./AsieLauncher/jarPatches");
 
 if(DO_LOCAL) {
   fs.writeFileSync("./ALWebFiles/also.json", JSON.stringify(infoData), {encoding: "utf-8"});
@@ -170,8 +105,6 @@ if(DO_LOCAL) {
     res.json(infoData);
   });
 }
-
-addDirectory("zips", "./AsieLauncher/zips");
 
 if(!DO_LOCAL) {
   app.listen(config.port);
