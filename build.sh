bc3fb0f489418ca2cd3b4dfdb1f47c331edfff08
asiekierka <asiekierka@gmail.com>
2013-09-14 15:53:39 +0200

    Semi-automate build process
    Client binary still has to be manually exported.

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-# TODO: BUILD CLIENT (needs port to Maven)
+rm -rf release/*.zip
+# Client
+cd release
+mkdir al
+cd al
+unzip ../AsieLauncher-latest.jar
+rm ../AsieLauncher-latest.jar
+rm -rf resources
+zip -9 -r ../AsieLauncher-latest.jar .
+cd ..
+rm -rf al
+cd ..
+# Server
 cd server/AsieLauncher/internal
 zip -9 -r ../../../release/AsieLauncher-latest-server.zip *.js
+# Bootstrap
+rm -rf launcher/*
+cd ../..
+rm -rf AsieLauncher/temp/*
+zip -9 -r ../release/AsieLauncher-latest-bootstrap.zip AsieLauncher also-config.json package.json also.js
