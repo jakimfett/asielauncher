afcc7be63143b41e7e96bdc4806e6f25539da491
jakimfett <jakimfett@picraft.com>
2014-07-11 23:49:12 -0700

    Remove ENTER key debug text, add key event listener

--- client/pl/asiekierka/AsieLauncher/launcher/gui/AsieLauncherGUI.java
+++ client/pl/asiekierka/AsieLauncher/launcher/gui/AsieLauncherGUI.java
@@ -201,10 +201,7 @@ public class AsieLauncherGUI extends JFrame implements IProgressUpdater
                 if (event.getKeyCode() == KeyEvent.VK_CONTROL)
                 {
                     setControl(true);
-                } else if (event.getKeyCode() == KeyEvent.VK_ENTER)
-                {
-                    System.out.println("'ENTER' key was pressed");
-                }
+                } 
             }
 
             @Override
@@ -230,6 +227,37 @@ public class AsieLauncherGUI extends JFrame implements IProgressUpdater
             {
                 beginInstallation();
             }
+        });
+        
+        loginField.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyPressed(KeyEvent event)
+            {
+                if (event.getKeyCode() == KeyEvent.VK_ENTER)
+                {
+                    System.out.println("'ENTER' key was pressed");
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent event)
+            {
+                if (event.getKeyCode() == KeyEvent.VK_ENTER)
+                {
+                    System.out.println("'ENTER' key was released");
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent event)
+            {
+                if (event.getKeyCode() == KeyEvent.VK_ENTER)
+                {
+                    System.out.println("'ENTER' key was typed");
+                }
+            }
+
         });
 
         statusLabel = new JLabel(Strings.READY + " (" + Strings.VERSION + ": " + AsieLauncher.VERSION_STRING + ")");
