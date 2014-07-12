48e085a0ccd542ecc2c255792e05acdaa90667da
jakimfett <jakimfett@picraft.com>
2014-07-11 22:17:28 -0700

    Adding additional debugging output for zipfile handling

--- client/pl/asiekierka/AsieLauncher/download/FileDownloaderZip.java
+++ client/pl/asiekierka/AsieLauncher/download/FileDownloaderZip.java
@@ -6,28 +6,44 @@ import pl.asiekierka.AsieLauncher.common.IProgressUpdater;
 import pl.asiekierka.AsieLauncher.common.Utils;
 import pl.asiekierka.AsieLauncher.launcher.Strings;
 
-public class FileDownloaderZip extends FileDownloaderHTTP {
-	public String unpackLocation;
-	
-	public FileDownloaderZip(String in, String out, String unpack, String md5, int filesize, boolean overwrite) {
-		super(in, out, md5, filesize, overwrite);
-		this.unpackLocation = unpack;
-	}
-	@Override
-	public boolean install(IProgressUpdater updater) {
-		boolean downloaded = super.install(updater, true);
-    	if(!downloaded) return false;
-    	File dirFile = new File(unpackLocation);
-    	dirFile.mkdir();
-		updater.setStatus(Strings.UNPACKING+" "+this.getFilename()+"...");
-    	try {
-    		Utils.extract(this.getLocation(), unpackLocation, this.isOverwrite());
-    	} catch(Exception e) { e.printStackTrace(); return false; }
-    	return true;
-	}
+public class FileDownloaderZip extends FileDownloaderHTTP
+{
+    public String unpackLocation;
 
-	@Override
-	public boolean remove() {
-		return true; // DUMMY! We can't really figure out what we removed, besides, users might have touched it.
-	}
+    public FileDownloaderZip(String in, String out, String unpack, String md5, int filesize, boolean overwrite)
+    {
+        super(in, out, md5, filesize, overwrite);
+        this.unpackLocation = unpack;
+    }
+
+    @Override
+    public boolean install(IProgressUpdater updater)
+    {
+        boolean downloaded = super.install(updater, true);
+        if (!downloaded)
+        {
+            return false;
+        }
+        File dirFile = new File(unpackLocation);
+        dirFile.mkdir();
+        updater.setStatus(Strings.UNPACKING + " " + this.getFilename() + "...");
+        try
+        {
+            System.out.println("Location: " + this.getLocation());
+            System.out.println("Destination: " + unpackLocation);
+            System.out.println("Overwrite? " + this.isOverwrite());
+            Utils.extract(this.getLocation(), unpackLocation, this.isOverwrite());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove()
+    {
+        return true; // DUMMY! We can't really figure out what we removed, besides, users might have touched it.
+    }
 }
